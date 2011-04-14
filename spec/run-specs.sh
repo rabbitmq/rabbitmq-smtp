27b96fdd16843b9db84d809250a69082d4465b57
Matthias Radestock <matthias@rabbitmq.com>
2011-04-14 15:28:14 +0100

    clearing 'junk' branch

--- spec/run-specs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-which spec >/dev/null
-[ "$?" = "1" ] && echo "Test cases require ruby and the rspec gem to be installed" && exit 1
-
-SPEC_DIR=`dirname $0`
-spec -c -f s $SPEC_DIR/adapter.rb
